4340699c9bdf424d86252842c400bc6ad30c979d
esjamesboot5 <estatesale480@gmail.com>
2025-07-08 06:29:08 -0700

    A:Initial game commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import pygame
+from constants import *
+from player import Player
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    pygame.display.set_caption("Asteroids")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.DOUBLEBUF)
+    dt = 0
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        #Limit the timeframe to 60 fps
+        dt = clock.tick(60) / 1000
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+if __name__ == "__main__":
+    main()
